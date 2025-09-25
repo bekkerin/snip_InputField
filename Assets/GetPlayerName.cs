213bf984670cb1e2b5f6a0db63444e75751709fe
Ernst Bekkering <bekkerin@users.noreply.github.com>
2025-09-25 13:33:34 -0500

    Initial commit

---
+++ Assets/GetPlayerName.cs
@@ -0,0 +1,23 @@
+﻿// this script is attached to the Playername Inputfield. The code in Displayname() executes in the On Value Changed method so every time another character is entered, the string variable and text of Showname are updated.
+// we make myPlayerName string static so it can be used in the next scene.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GetPlayerName : MonoBehaviour
+{
+    public InputField PlayerName;
+    public static string myPlayerName;
+    public Text ShowName;
+
+
+
+    public void DisplayName()
+    {
+        myPlayerName = PlayerName.text; // assign input to string
+        ShowName.text = myPlayerName; //assign string to textbox
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# snip_InputField
+simple program showing the functionality of the inputfield
